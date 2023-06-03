0ce70d87a949dd1dd7586d8252cfa89f7e1aaa1a
Franco Paesani Rosello <francopaesanirosello@gmail.com>
2023-06-03 13:23:25 -0300

    let intentos

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function validarDatos() {
+    let intentos = 0
+    let cambio 
 
-    let cambio = prompt('Que desea convertir? \n (1 U$D = $500) \n (D) Dolares americanos a pesos argentinos \n (P) Pesos argentinos a dolares americanos').toUpperCase()
+    while (intentos < 3) {
 
-    if (cambio === "D") {
-        cambioDolar()
-    } else if (cambio === "P") {
-        cambioPesos()                                                           
-    } else {
-        alert('ingrese solo "D" o "P" sin comillas')
+        cambio = prompt('Que desea convertir? \n (1 U$D = $500) \n (D) Dolares americanos a pesos argentinos \n (P) Pesos argentinos a dolares americanos').toUpperCase()
+
+        if (cambio === "D") {
+            cambioDolar()
+        } else if (cambio === "P") {
+            cambioPesos()
+        } else {
+            alert('ingrese solo "D" o "P" sin comillas')
+            intentos++
+        }
+    }
+
+    if (intentos === 3) {
+        alert('La "D" esta al lado de la "S" y la "P" esta pegada pegada a la "O", no es tan dificil')
         validarDatos()
     }
 }
@@ -22,7 +32,7 @@ function cambioDolar() {
     } else if (dolar === 0) {
         validarDatos()
     } else {
-        alert('La cantidad en pesos seria de' + ' ' + (dolar*500))
+        alert('La cantidad en pesos seria de' + ' ' + (dolar * 500))
     }
 }
 
@@ -30,14 +40,14 @@ function cambioDolar() {
 function cambioPesos() {
 
     let pesos = Number(prompt('Ingrese la cantidad de pesos sin decimales o "0" para voler al menu'))
-    
-    if (isNaN (pesos)) {
+
+    if (isNaN(pesos)) {
         alert('ingrese cifra numerica')
         cambioPesos()
     } else if (pesos === 0) {
         validarDatos()
-    }else {
-        alert('La cantidad en dolar blue seria de' + ' ' + (pesos/500))
+    } else {
+        alert('La cantidad en dolar blue seria de' + ' ' + (pesos / 500))
     }
 }
 
